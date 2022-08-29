4efd88693c7f308ba83f72145246611991dd48ae
yeabsiramoges <yeab@mit.edu>
2022-08-29 13:52:36 -0400

    svm architecture installed

--- code/classifier.py
+++ code/classifier.py
@@ -25,6 +25,18 @@ dataset = dataframe.values
 
 x1 = numpy.asarray(dataset[:,0:1])
 x2 = numpy.asarray(dataset[:,1:2])
-x3 = numpy.asarray(dataset[:,2:3])
-y = numpy.asarray(dataset[:,3])
+y = numpy.asarray(dataset[:,2])
 
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2)
+
+svc = SVC()
+svc.fit(x1_train, x2_train, y_train)
+score = svc.score(x1_train, x2_train, y_train)
+
+cross_val = cross_val_score(svc, x1_train, x2_train, y_train, cv=10)
+
+y_pred = svc.predict(x1_test, x2_test)
+con_matrix = confusion_matrix(y_test, y_pred)
+classification = classification_report(y_test, y_pred)
+
+print(y_pred, con_matrix, classification)
