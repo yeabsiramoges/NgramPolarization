54ece334f2a689f2eb3eb8007bef348622556447
yeabsiramoges <yeab@mit.edu>
2022-08-29 13:14:44 -0400

    convering to using an svm

--- code/classifier.py
+++ code/classifier.py
@@ -1,4 +1,5 @@
 #Used for loading the csv data
+from telnetlib import X3PAD
 import pandas
 import numpy
 
@@ -9,34 +10,21 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 
 #Used for data preparation and model evaluation
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 BIGRAMS_FILE_PATH = r"bigrams.csv"
 
 dataframe = pandas.read_csv(BIGRAMS_FILE_PATH, header=None)
 dataset = dataframe.values
 
-X = numpy.asarray(dataset[:,0:3])
-Y = numpy.asarray(dataset[:,3])
+x1 = numpy.asarray(dataset[:,0:1])
+x2 = numpy.asarray(dataset[:,1:2])
+x3 = numpy.asarray(dataset[:,2:3])
+y = numpy.asarray(dataset[:,3])
 
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
-
-test_Y = numpy.asarray(np_utils.to_categorical(encoded_Y))
-
-def model():
-    model = Sequential()
-    model.add(Dense(8, input_dim=4, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-estimator = KerasClassifier(build_fn=model, epochs=200, batch_size=5, verbose=0)
-kfold = KFold(n_splits=10, shuffle=True)
-results = cross_val_predict(estimator, X, test_Y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
