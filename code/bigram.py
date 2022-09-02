9a31882d9d694d7e8d5b63bd78a85e8d7b29d5e6
yeabsiramoges <yeab@mit.edu>
2022-09-01 20:15:56 -0400

    added extra getters and setters for bigram class

--- code/bigram.py
+++ code/bigram.py
@@ -13,5 +13,11 @@ class Bigram:
     def get_bigram_string(self):
         return self.bigram_string
 
+    def get_misinformation_count(self):
+        return self.misinformation_count
+
+    def get_informative_count(self):
+        return self.informative_count
+
     def compare_to(self, other):
         return self.get_bigram_string() == other.get_bigram_string()
