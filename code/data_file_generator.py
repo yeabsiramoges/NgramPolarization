5fa02382dfd04bd76c0db309119bd2c85acae6c7
yeabsiramoges <yeab@mit.edu>
2022-11-21 04:14:53 -0500

    added csv and json readers

--- code/data_file_generator.py
+++ code/data_file_generator.py
@@ -11,6 +11,26 @@ def generate_csv(adfontes_links, adfontes_text):
                 text = text.replace("\n"," ")
                 writer.writerow((link, text))
 
+def extact_csv_data(adfontes_csv_file=ADFONTES_CSV_FILE):
+    csv_dict = {
+        'links':[],
+        'text':[]
+    }
+    with open(adfontes_csv_file, "r", encoding="UTF-8") as csv_file:
+        for line in csv_file:
+            line_list = line.split(",")
+            link = line_list[0]
+            text = "".join(line_list[1:])
+
+            csv_dict['links'].append(link)
+            csv_dict['text'].append(text)
+    return csv_dict
+
+
 def generate_json(adfontes_dict):
     with open(ADFONTES_JSON_FILE, "w", encoding='UTF-8') as json_file:
         json.dump(adfontes_dict, json_file)
+    
+def extract_dict(adfontes_json_file=ADFONTES_JSON_FILE):
+    with open(adfontes_json_file, 'r', encoding='UTF-8') as json_file:
+        return json.load(json_file)
